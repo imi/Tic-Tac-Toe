db216cc5ed510876b7d2b49494669d9e472bc69c
Eyad <81812181+imi@users.noreply.github.com>
2022-06-19 07:26:47 +0300

    Add files via upload

---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Label[,] board;
+        public bool player_turn = true;
+        public bool lost = false;
+        public int comp_wins = 0, player_wins = 0, tie = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            board = new Label[3, 3] {
+                {label5, label6, label7},
+                {label8, label9, label10},
+                {label11, label12, label13}
+            };
+        }
+
+        public void ai_move()
+        {
+            string[,] sboard = Minimax.CreateString(board);
+            Minimax.minimax(sboard, 20, false, true);
+            Label label = board[Minimax.next_pos[0], Minimax.next_pos[1]];
+            label.Text = "O";
+            get_lose();
+            if (!lost) player_turn = true;
+        }
+
+        public void get_lose() {
+            string[,] sboard = Minimax.CreateString(board);
+            int result = Minimax.Winner(sboard);
+            if (result != 1)
+            {
+                lost = true;
+                if (result == -2)
+                {
+                    MessageBox.Show("You Lost Onii-Chan");
+                    comp_wins += 1;
+                    label20.Text = comp_wins.ToString();
+                }
+                if (result == 2){
+                    MessageBox.Show("You Won Onii-Chan");
+                    player_wins += 1;
+                    label19.Text = player_wins.ToString();
+                }
+                if (result == 0) {
+                    MessageBox.Show("Tie Onii-Chan");
+                    tie += 1;
+                    label22.Text = tie.ToString();
+                }
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            if (player_turn) {
+                if (label5.Text.Length == 0)
+                {
+                    label5.Text = "X";
+                    get_lose();
+                    if (!lost) { player_turn = false; ai_move(); };
+                }
+                else MessageBox.Show("Place is Not Empty");
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            if (player_turn)
+            {
+                if (label6.Text.Length == 0)
+                {
+                    label6.Text = "X";
+                    get_lose();
+                    if (!lost) { player_turn = false; ai_move(); };
+                }
+                else MessageBox.Show("Place is Not Empty");
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            if (player_turn)
+            {
+                if (label7.Text.Length == 0)
+                {
+                    label7.Text = "X";
+                    get_lose();
+                    if (!lost) { player_turn = false; ai_move(); };
+                }
+                else MessageBox.Show("Place is Not Empty");
+            }
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            if (player_turn)
+            {
+                if (label8.Text.Length == 0)
+                {
+                    label8.Text = "X";
+                    get_lose();
+                    if (!lost) { player_turn = false; ai_move(); };
+                }
+                else MessageBox.Show("Place is Not Empty");
+            }
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            if (player_turn)
+            {
+                if (label9.Text.Length == 0)
+                {
+                    label9.Text = "X";
+                    get_lose();
+                    if (!lost) { player_turn = false; ai_move(); };
+                }
+                else MessageBox.Show("Place is Not Empty");
+            }
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+            if (player_turn)
+            {
+                if (label10.Text.Length == 0)
+                {
+                    label10.Text = "X";
+                    get_lose();
+                    if (!lost) { player_turn = false; ai_move(); };
+                }
+                else MessageBox.Show("Place is Not Empty");
+            }
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+            if (player_turn)
+            {
+                if (label11.Text.Length == 0)
+                {
+                    label11.Text = "X";
+                    get_lose();
+                    if (!lost) { player_turn = false; ai_move(); };
+                }
+                else MessageBox.Show("Place is Not Empty");
+            }
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+            if (player_turn)
+            {
+                if (label12.Text.Length == 0)
+                {
+                    label12.Text = "X";
+                    get_lose();
+                    if (!lost) { player_turn = false; ai_move(); };
+                }
+                else MessageBox.Show("Place is Not Empty");
+            }
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+            if (player_turn)
+            {
+                if (label13.Text.Length == 0)
+                {
+                    label13.Text = "X";
+                    get_lose();
+                    if (!lost) { player_turn = false; ai_move(); };
+                }
+                else MessageBox.Show("Place is Not Empty");
+            }
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void label17_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    Label label = board[i, j];
+                    label.Text = "";
+                    player_turn = true;
+                    lost = false;
+                }
+            }
+        }
+    }
+}
---
+++ TicTacToe/Minimax.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public class Minimax
+    {
+        public static List<int> next_pos;
+        public static int minimax(string[,] board, int depth, bool max, bool turn = false) {
+            int result = Winner(board);
+            if (depth == 0 || result != 1) {  return result; }
+            if (max)
+            {
+                int value = -10;
+                foreach (List<int> list in emptySpaces(board))
+                {
+                    board[list[0], list[1]] = "X";
+                    int new_value = minimax(board, depth - 1, false);
+                    board[list[0], list[1]] = "";
+                    if (new_value > value)
+                    {
+                        value = new_value;
+                    };
+                }
+                return value;
+            }
+            else
+            {
+                int value = 10;
+                foreach (List<int> list in emptySpaces(board))
+                {
+                    board[list[0], list[1]] = "O";
+                    int new_value = minimax(board, depth - 1, true);
+                    board[list[0], list[1]] = "";
+                    if (new_value < value)
+                    {
+                        value = new_value;
+                        if (turn) next_pos = list;
+                    };
+                }
+                return value;
+            }
+        }
+
+        public static int Winner(string[,] board) {
+            // Rows
+            for (int i = 0; i < 3; i++) {
+                if (board[i, 0] == "X" && board[i, 1] == "X" && board[i, 2] == "X") return 2;
+                else if (board[i, 0] == "O" && board[i, 1] == "O" && board[i, 2] == "O") return -2;
+            }
+            // Columns
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[0, i] == "X" && board[1, i] == "X" && board[2, i] == "X") return 2;
+                else if (board[0, i] == "O" && board[1, i] == "O" && board[2, i] == "O") return -2;
+            }
+            // Dims
+            if (board[0, 2] == "X" && board[1, 1] == "X" && board[2, 0] == "X") return 2;
+            else if (board[0, 2] == "O" && board[1, 1] == "O" && board[2, 0] == "O") return -2;
+            if (board[0, 0] == "X" && board[1, 1] == "X" && board[2, 2] == "X") return 2;
+            else if (board[0, 0] == "O" && board[1, 1] == "O" && board[2, 2] == "O") return -2;
+            // Tie
+            bool tie = true;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    string pos = board[i, j];
+                    if (pos.Length == 0) tie = false;
+                }
+            }
+            if (tie) return 0;
+            return 1;
+        }
+
+        public static List<List<int>> emptySpaces(string[,] board) {
+            List<List<int>> position = new List<List<int>>();
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    String elm = board[i,j];
+                    if (elm.Length == 0) position.Add(new List<int>() { i, j });
+                }
+            }
+            return position;
+        }
+
+        public static string[,] CreateString(Label[,] board){
+            string[,] StringBoard = new string[3, 3];
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    Label label = board[i,j];
+                    StringBoard[i, j] = label.Text;
+                }
+            }
+            return StringBoard;
+        }
+    }
+}
